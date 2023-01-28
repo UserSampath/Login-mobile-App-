aae0fb6e1a1cd0adc492da3dffd7b71233fa900b
UserSampath <nalakasampathsmp@gmail.com>
2023-01-28 06:36:23 +0530

    second commit

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -8,18 +8,21 @@ class MyButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
+      
       child: Container(
         padding: const EdgeInsets.all(25),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
             color: Colors.blue[500], borderRadius: BorderRadius.circular(10)),
         child: const Center(
-            child: Text(
-          'Sign in',
-          style: TextStyle(
-              color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
-        )),
+          child: Text(
+            'Sign in',
+            style: TextStyle(
+                color: Colors.white, fontSize: 18, fontWeight: FontWeight.bold),
+          ),
+        ),
       ),
+      
     );
   }
 }
